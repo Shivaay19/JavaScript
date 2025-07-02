b5a3c83381529d20e8ef7182c047923bef3e53bf
Shivaay19 <zephyrus1920@gmail.com>
2025-07-02 23:51:05 +0530

    Commit

---
+++ variables.js
@@ -0,0 +1,10 @@
+const accountName = "Prakhand Patel";  //cannot be changed further
+let age  = 22;     //variable
+var city = "Lucknow";    //variable but with scoping issue, prefer not to use
+state = "Uttar Pradesh";    //automatic type identification
+
+let income;   //Undefined Variable
+
+console.log(accountName);
+
+console.table([accountName,age,city,state,income]);
